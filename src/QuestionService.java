d6e9229b3757d915c72063074f95b1c371815c57
UnfazedAy <ayomidesoniyii@gmail.com>
2024-02-07 09:05:16 +0100

    Make user be able to answer and get their total score

--- src/QuestionService.java
+++ src/QuestionService.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public  class QuestionService {
     Question[] questions = new Question[5];
+    String[] selection = new String[5];
     public QuestionService() {
         questions[0] = new Question(
                 1,
@@ -48,9 +51,33 @@ public  class QuestionService {
         );
     }
 
-    public void displayQuestions() {
+    public void playQuiz  () {
+        int i = 0;
         for (Question q : questions) {
-            System.out.println(q);
+            System.out.printf("Question no. : %d\n", q.getId());
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+            Scanner sc = new Scanner(System.in);
+            selection[i] = sc.nextLine();
+            i += 1;
         }
+        for (String s: selection) {
+            System.out.println(s);
+        }
+    }
+    public void printScore() {
+        int score = 0;
+        for (int i =0; i < questions.length; i++) {
+            Question que = questions[i];
+            String actualAnswer = que.getAnswer();
+            String userAnswer = selection[i];
+            if (actualAnswer.equals(userAnswer)) {
+                score += 1;
+            }
+        }
+        System.out.printf("Your score is : %d", score);
     }
 }
